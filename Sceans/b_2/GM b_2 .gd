2f8087cf4a88f85f31253c2d5668ff3b76d989d3
Drake <mkanyal001@gmail.com>
2024-11-26 20:39:48 +0530

    imprr

--- Sceans/b_2/GM b_2 .gd
+++ Sceans/b_2/GM b_2 .gd
@@ -1,6 +1,7 @@
 extends Node
 @onready var striker = $"../striker"
 @onready var canvas_modulate = $"../Bg/ParallaxBackground/CanvasModulate"
+@onready var canvas_modulate_2 = $"../TileMap2/CanvasModulate2"
 
 
 # Called when the node enters the scene tree for the first time.
@@ -10,6 +11,7 @@ var camera_limits = [ 0, -10000000, -10000000 , 170 ]
 #                    left, top , left, bottom 
 func _ready():
 	canvas_modulate.visible = true
+	canvas_modulate_2.visible = true
 func _on_kill_zone_body_entered(body):
 	pass # Replace with function body.
 	if body.is_in_group("player"):
--- Sceans/game_obj/DayNightCycke/for tile/CanvasModulate2.gd
+++ Sceans/game_obj/DayNightCycke/for tile/CanvasModulate2.gd
@@ -2,11 +2,11 @@ extends CanvasModulate
 
 var time:float = 0.0 
 # Called when the node enters the scene tree for the first time.
-@export var gradient :GradientTexture1D
+@export var gradient2 :GradientTexture1D
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
-	time += delta/9
+	time += delta/3
 	var value = (sin(time - PI /2)+1.0)/5.0
-	self.color = gradient.gradient.sample(value)
+	self.color = gradient2.gradient.sample(value)
